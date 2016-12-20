e3c72089f7f3e34abc2c2e11a16fcd6f01227ab1
PATRICK-PC\bchen <bchen09@syr.edu>
2016-12-19 20:01:06 -0500

    check-point-16

--- db/migrate/20161220003207_create_comments.rb
+++ db/migrate/20161220003207_create_comments.rb
@@ -1,9 +1,8 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.test :body
+      t.text :body
       t.references :post, index: true, foreign_key: true
-
       t.timestamps null: false
     end
   end
