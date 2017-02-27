2c4ac8925ef7379f6856289ba4703512582bca03
PATRICK-PC\bchen <bchen09@syr.edu>
2017-02-26 22:04:23 -0700

    checkpoint-22-compelted

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,37 @@
 require 'random_data'
 
- # Create Posts
- 50.times do
- # #1
-   Post.create!(
- # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+# Create Topics
+15.times do
+    Topic.create!(
+        name:         RandomData.random_sentence,
+        description:  RandomData.random_paragraph
+    )
+end
+topics = Topic.all
 
- # Create Comments
- # #3
- 100.times do
-   Comment.create!(
- # #4
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+# Create Posts
+50.times do
+    # #1
+    Post.create!(
+        # #2
+        topic:  topics.sample,
+        title:  RandomData.random_sentence,
+        body:   RandomData.random_paragraph
+    )
+end
+posts = Post.all
 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+# Create Comments
+# #3
+100.times do
+    Comment.create!(
+        # #4
+        post: posts.sample,
+        body: RandomData.random_paragraph
+    )
+end
+
+puts 'Seed finished'
+puts "#{Topic.count} topics created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
