81c1da3a0ac17e2c802dea9ee9d5f80cee5b93d7
PATRICK-PC\bchen <bchen09@syr.edu>
2017-03-09 17:47:08 -0700

    assignment-24-completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,26 @@
 class User < ActiveRecord::Base
-  # #2
-  before_save { self.email = email.downcase if email.present? }
+  before_save { self.email = email.downcase }
+  before_save  :format_name
 
-# #3
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-# #4
- validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
- validates :password, length: { minimum: 6 }, allow_blank: true
-# #5
+  validates :password, presence: true, length: { minimum: 6 }
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
-            length: { minimum: 3, maximum: 254 }
+            length: { minimum: 3, maximum: 100 },
+            format: { with: EMAIL_REGEX }
 
-# #6
   has_secure_password
+
+  def format_name
+    if name
+     name_array = []
+     name.split.each do |name_part|
+       name_array << name_part.capitalize
+     end
+       self.name = name_array.join(" ")
+    end
+  end
 end
