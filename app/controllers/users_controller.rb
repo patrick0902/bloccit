3c0271283e3e309dbb5c635bdff8a178225960dc
PATRICK-PC\bchen <bchen09@syr.edu>
2017-03-09 17:56:10 -0700

    assignment-25-completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,29 @@
 class UsersController < ApplicationController
   def new
-  @user = User.new
-end
-def create
-# #9
-   @user = User.new
-   @user.name = params[:user][:name]
-   @user.email = params[:user][:email]
-   @user.password = params[:user][:password]
-   @user.password_confirmation = params[:user][:password_confirmation]
+    @user = User.new
+  end
 
-# #10
-   if @user.save
-     flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-     redirect_to root_path
-   else
-     flash.now[:alert] = "There was an error creating your account. Please try again."
-     render :new
-   end
- end
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end
 
+  def create
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to root_path
+    else
+      flash[:error] = "There was an error creating your account. Please try again."
+      render :new
+    end
+  end
 end
