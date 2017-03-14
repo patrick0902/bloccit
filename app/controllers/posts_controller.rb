0b136070e3bd5102d6eb9d0607a78800bff7885d
PATRICK-PC\bchen <bchen09@syr.edu>
2017-03-13 22:17:22 -0600

    checkpoint-27-completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,64 +1,60 @@
 class PostsController < ApplicationController
+  before_action :require_sign_in, except: :show
 
   def show
-     @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
-    # #7
     @topic = Topic.find(params[:topic_id])
-  @post = Post.new
-
+    @post = Post.new
   end
 
   def create
-# #9
-  @post = Post.new
-  @post.title = params[:post][:title]
-  @post.body = params[:post][:body]
-  @topic = Topic.find(params[:topic_id])
-# #35
-  @post.topic = @topic
-# #10
-  if @post.save
-# #11
-    flash[:notice] = "Post was saved."
-           redirect_to [@topic, @post]
-  else
-# #12
-    flash.now[:alert] = "There was an error saving the post. Please try again."
-    render :new
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.build(post_params)
+    @post.user = current_user
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
-end
 
   def edit
-     @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
-             redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
-      flash.now[:alert] = "There was an error saving the post. Please try again."
+      flash[:error] = "There was an error saving the post. Please try again."
       render :edit
     end
   end
 
   def destroy
-  @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
 
-# #8
-  if @post.destroy
-    flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-    redirect_to @post.topic
-  else
-    flash.now[:alert] = "There was an error deleting the post."
-    render :show
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to @post.topic
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
   end
-end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'random_data'
-
+# Create Users
+5.times do
+  User.create!(
+# #3
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
 # Create Topics
 15.times do
     Topic.create!(
@@ -14,6 +23,7 @@ topics = Topic.all
     # #1
     Post.create!(
         # #2
+        user:   users.sample,
         topic:  topics.sample,
         title:  RandomData.random_sentence,
         body:   RandomData.random_paragraph
@@ -30,8 +40,13 @@ posts = Post.all
         body: RandomData.random_paragraph
     )
 end
-
+user = User.first
+user.update_attributes!(
+  email: 'youremail.com', # replace this with your personal email
+  password: 'helloworld'
+)
 puts 'Seed finished'
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
